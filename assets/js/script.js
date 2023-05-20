0f4bd1ff7c16b6f044ee68319bf59d9a1dd0d473
Rafaelocm <rafaelrascado98@gmail.com>
2023-05-20 00:19:12 -0300

    refactor: alterando o contato para o da empresa

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.querySelector('#submit').addEventListener('click', function () {
     let cpf = document.querySelector('#cpf').value;
     let assunto = document.querySelector('#assunto').value;
 
-    let url = "https://api.whatsapp.com/send?phone=557193456144&text= Nome? %0A " + nome + "%0A Qual o seu e-mail %0A " + email + " %0A Qual o seu cnpj?%0A" +
+    let url = "https://api.whatsapp.com/send?phone=557196357878&text= Nome? %0A " + nome + "%0A Qual o seu e-mail %0A " + email + " %0A Qual o seu cnpj?%0A" +
         cnpj + " %0A Qual o seu cpf?%0A" + cpf + " %0A Assunto ou itens: %0A" + assunto;
 
     window.open(url);
